e5f9f78bb169ce4ac6d920f15935cf30076d5b1c
KeziahMoselle <keziah.moselle@gmail.com>
2020-03-02 23:49:37 +0100

    feat: intersection observer scroll spy for navbar links

--- script/main.js
+++ script/main.js
@@ -6,29 +6,67 @@ $(document).ready(function () {
 // Menu -----------------------------------------------
 const navbar = document.querySelector('nav');
 
+
 window.addEventListener('scroll', () => {
   if (window.scrollY > 50) {
-    navbar.classList.remove('white')
+    navbar.classList.remove('white');
   } else if (window.scrollY < 80) {
-    navbar.classList.add('white')
+    navbar.classList.add('white');
   }
 });
 
 
-window.addEventListener('scroll', event => {
-  const navigationLinks = document.querySelectorAll('nav ul li a');
-  const fromTop = window.scrollY;
- 
-  navigationLinks.forEach(link => {
-    const section = document.querySelector(link.hash);
-   
-    if (
-      section.offsetTop <= fromTop &&
-      section.offsetTop + section.offsetHeight > fromTop
-    ) {
-      link.classList.add('active');
-    } else {
-      link.classList.remove('active');
-    }
+const navigationLinks = document.querySelectorAll('.menu a');
+const sections = [...navigationLinks].map(
+  link => document.querySelector(link.hash)
+);
+
+if (sections.length > 0) {
+  // Observer options
+  const ratio = 0.4;
+  const y = Math.round(window.innerHeight * ratio);
+
+  // Create our observer
+  const observer = new IntersectionObserver(callback, {
+    rootMargin: `-${window.innerHeight - y - 1}px 0px -${y}px 0px`
   });
-});
+
+  // Observe our sections
+  sections.forEach(section => observer.observe(section));
+
+
+  /**
+   *
+   *
+   * @param {IntersectionObserverEntry[]} entries
+   * @param {IntersectionObserver} observer
+   */
+  function callback (entries, observer) {
+    entries.forEach(entry => {
+      // If the section is in the intersectionRatio, activate it
+      if (entry.isIntersecting) {
+        activate(entry.target);
+      }
+    })
+  }
+
+
+  /**
+   * Add .active class to the navbar anchor
+   *
+   * @param {HTMLElement} element
+   */
+  function activate (element) {
+    // Find the anchor element in the navbar
+    const id = element.id;
+    const navLink = document.querySelector(`a[href="#${id}"]`);
+    
+    // Remove already active anchors
+    navLink.parentElement.parentElement
+      .querySelectorAll('.active')
+      .forEach(node => node.classList.remove('active'));
+
+    // Add .active class to the anchor
+    navLink.classList.add('active');
+  }
+}
